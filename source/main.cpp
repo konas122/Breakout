6a53d8cca1518fccbfbdd46901e4e99eb9419737
konas122 <2407626470@qq.com>
2024-03-04 22:46:12 +0800

    update

--- source/main.cpp
+++ source/main.cpp
@@ -1,66 +1,56 @@
-#define GLEW_STATIC
-#include <GL/glew.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include "game.h"
 #include "resource_manager.h"
 
+#include <iostream>
+
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-const GLuint SCREEN_WIDTH = 800;
-const GLuint SCREEN_HEIGHT = 600;
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
 
 Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
-int main(int argc, char *argv[]) {
+int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
 
     GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
-    glewExperimental = GL_TRUE;
-    glewInit();
-    glGetError(); // Call it once to catch glewInit() bug, all other errors are now from our application.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
     glfwSetKeyCallback(window, key_callback);
 
-    // OpenGL configuration
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    glEnable(GL_CULL_FACE);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // Initialize game
     Breakout.Init();
 
-    // DeltaTime variables
-    GLfloat deltaTime = 0.0f;
-    GLfloat lastFrame = 0.0f;
-
-    // Start Game within Menu State
-    Breakout.State = GAME_ACTIVE;
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
 
     while (!glfwWindowShouldClose(window)) {
-        // Calculate delta time
-        GLfloat currentFrame = glfwGetTime();
+        float currentFrame = (float)glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
         glfwPollEvents();
 
-        //deltaTime = 0.001f;
-        // Manage user input
         Breakout.ProcessInput(deltaTime);
 
-        // Update Game state
         Breakout.Update(deltaTime);
 
-        // Render
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         Breakout.Render();
@@ -68,7 +58,6 @@ int main(int argc, char *argv[]) {
         glfwSwapBuffers(window);
     }
 
-    // Delete all resources as loaded using the resource manager
     ResourceManager::Clear();
 
     glfwTerminate();
@@ -77,14 +66,13 @@ int main(int argc, char *argv[]) {
 
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    // When a user presses the escape key, we set the WindowShouldClose property to true, closing the application
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    if (key >= 0 && key < 1024)
-    {
+        glfwSetWindowShouldClose(window, true);
+    if (key >= 0 && key < 1024) {
         if (action == GLFW_PRESS)
-            Breakout.Keys[key] = GL_TRUE;
-        else if (action == GLFW_RELEASE)
-            Breakout.Keys[key] = GL_FALSE;
+            Breakout.Keys[key] = true;
+        else if (action == GLFW_RELEASE) {
+            Breakout.Keys[key] = false;
+        }
     }
 }
